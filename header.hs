45629c7d9d206d01d022e89f361c4c4bee60b911
Peter Brindisi <peter@indiegogo.com>
2015-07-08 19:20:44 -0700

    implement decodeHeader

--- header.hs
+++ header.hs
@@ -4,6 +4,7 @@ import Data.Aeson
 import Control.Applicative
 import Control.Monad
 import Data.Time.LocalTime
+import Data.ByteString.Lazy
 
 data Header = Header {
   getTitle :: String,
@@ -14,5 +15,13 @@ instance FromJSON Header where
   parseJSON (Object json) = Header <$>
                             json .: "title" <*>
                             json .: "publish_at"
-
   parseJSON _ = mzero
+
+modifyPublishAt :: Header -> ZonedTime -> Header
+modifyPublishAt header time = header { getPublishAt = time }
+
+decodeHeader :: ByteString -> TimeZone -> Maybe Header
+decodeHeader json tz = toUTC <$> decode json
+  where toUTC header = modifyPublishAt header $ convert $ getPublishAt header
+        convert = utcToZonedTime utc . localTimeToUTC tz . zonedTimeToLocalTime
+
